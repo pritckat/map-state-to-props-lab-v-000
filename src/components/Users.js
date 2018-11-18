fb82adedf090d3a16c3d3036d7817ae042c624e4
Katrina Pritchard <pritckat@gmail.com>
2018-11-18 16:58:08 +0000

    automatically backed up by learn

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
 class Users extends Component {
 
   render() {
+    let users = this.props.users.map((user, index) => <li key={index}>{user.username}</li>)
     return (
       <div>
+        {this.props.numberOfUsers}
         <ul>
-          Users!
+          {users}
         </ul>
       </div>
     )
